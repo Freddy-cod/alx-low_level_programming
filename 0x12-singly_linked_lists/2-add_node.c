2e62e4acbdc220f6d2897429dfc2d196e55d3ddb
Freddy-cod <alfredphronesis@outlook.com>
2023-07-28 07:25:45 -0700

    Fixed a of the errors in the code

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,32 +3,44 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: pointer to the list_t list
- * @str: new string to add in the node
+ * add_node - Inserts a new node at the beginning of a linked list.
+ * @head: Double pointer to the list_t list.
+ * @str: New string to store in the node.
  *
- * Return: the address of the new element, or NULL if it fails
+ * Return: Pointer to the newly added node, or NULL if it fails.
  */
-list_t *add_node(list_t *head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	size_t len = strlen(str);
+	/* Declare a new node pointer and a variable to store the length of str */
+	list_t *new_node;
+	unsigned int str_len = 0;
 
-	/* Declare new node */
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	/* Calculate the length of the input string */
+	while (str[str_len])
+		str_len++;
+
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
+
+	if (!new_node)
 		return (NULL);
 
-	/* Initialize new node */
-	new->str = strdup(str);
-	if (new->str == NULL)
+	/* Duplicate the input string using strdup */
+	new_node->str = strdup(str);
+
+	if (!new_node->str) /* Check if strdup failed */
 	{
-		free(new);
+		free(new_node); /* Free the newly created node if strdup fails */
 		return (NULL);
 	}
 
-	new->len = len;
-	new->next = head;
+	/* Initialize the new node with the given values */
+	new_node->len = str_len;
+	new_node->next = (*head);
 
-	return (new);
+	/* Update the head to point to the new node, making it the new head */
+	(*head) = new_node;
+
+	return (*head);
 }
+
