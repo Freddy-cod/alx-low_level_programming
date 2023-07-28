ad81470cf9686d3035c45f9ab8e1819a219e0832
Freddy-cod <alfredphronesis@outlook.com>
2023-07-28 05:25:02 -0700

    Fixed the styling of the code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,26 +9,26 @@
  */
 size_t print_list(const list_t *h)
 {
-    /* Base case: If the current node is NULL, the list is empty, return 0. */
-    if (h == NULL)
-    {
-        return 0;
-    }
+	/* Base case: If the current node is NULL, the list is empty, return 0. */
+	if (h == NULL)
+	{
+		return (0);
+	}
 
-    /* Print the current node's data */
-    if (h->str == NULL)
-    {
-        printf("[0] (nil)\n"); /* If the string is NULL, print [0] (nil) */
-    }
-    else
-    {
-        printf("[%u] %s\n", h->len, h->str); /* Print the length and the string */
-    }
+	/* Print the current node's data */
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n"); /* If the string is NULL, print [0] (nil) */
+	}
+	else
+	{
+		printf("[%u] %s\n", h->len, h->str); /* Print the length and the string */
+	}
 
-    /* Make a recursive call to print_list with the next node (h->next). */
-    /* The recursive call will handle the rest of the list. */
-    /* The return value of the recursive call will be the number of nodes */
-    /* printed in the remaining part of the list. */
-    return 1 + print_list(h->next);
+	/* Make a recursive call to print_list with the next node (h->next). */
+	/* The recursive call will handle the rest of the list. */
+	/* The return value of the recursive call will be the number of nodes */
+	/* printed in the remaining part of the list. */
+	return (1 + print_list(h->next));
 }
 
