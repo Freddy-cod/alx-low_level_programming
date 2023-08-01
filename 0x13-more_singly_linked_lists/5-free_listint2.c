9e90b02415a06215f7aa509dc6d154f90f0977d1
Freddy-cod <alfredphronesis@outlook.com>
2023-08-01 00:50:54 -0700

    Fixed the firsth task errors

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,25 +1,24 @@
 #include "lists.h"
 
 /**
- * free_listint2 - This Frees a linked list.
- * @head: Double pointer to the listint_t list to be freed.
+ * free_listint2 - Frees a linked list,sets pointer to NULL.
+ * @head: Pointer to the listint_t list to be freed.
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
+
+	/* Check if list is empty or  NULL. */
 	if (head == NULL || *head == NULL)
 		return;
 
-	listint_t *current_node;
-	listint_t *next_node;
-
-	current_node = *head;
-
-	while (current_node)
+	while (*head)
 	{
-		next_node = current_node->next; /* Store the next node */
-		free(current_node); /* Free the current node */
-		current_node = next_node; /* Move to the next node */
+		temp = (*head)->next; /* Store the next node */
+
+		free(*head); /* Free the current node */
+		*head = temp; /* Move to the next node */
 	}
 
-	*head = NULL; /* Reset the head pointer to NULL */
+	*head = NULL; /* Set the head to NULL */
 }
